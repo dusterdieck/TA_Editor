e971e7cbfff209a8c15d99962fb27289afd91eda
Michael Heasell <michael.heasell@gmail.com>
2019-06-21 20:44:57 +0100

    Add TDF code from TAUtil

---
+++ TA_Editor/Tdf/TdfNode.cs
@@ -0,0 +1,153 @@
+﻿namespace TAUtil.Tdf
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+
+    /// <summary>
+    /// Tree structure representing a hierarchy of keys and values
+    /// as in a TDF format file.
+    /// </summary>
+    public class TdfNode
+    {
+        private const int IndentationLevel = 4;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TdfNode"/> class.
+        /// The created node has a name of null.
+        /// </summary>
+        public TdfNode()
+            : this(null)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TdfNode"/> class.
+        /// </summary>
+        /// <param name="name">The name of the node.</param>
+        public TdfNode(string name)
+        {
+            this.Name = name;
+            this.Keys = new Dictionary<string, TdfNode>(StringComparer.OrdinalIgnoreCase);
+            this.Entries = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+        }
+
+        /// <summary>
+        /// Gets or sets the name of the node.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets the mapping of keys in this node.
+        /// Each key maps to a TdfNode with the same name representing a child block.
+        /// </summary>
+        public Dictionary<string, TdfNode> Keys { get; private set; }
+
+        /// <summary>
+        /// Gets the mapping of entries in this node.
+        /// Each item is a property with a name and associated value.
+        /// </summary>
+        public Dictionary<string, string> Entries { get; private set; }
+
+        /// <summary>
+        /// Reads a TDF file from the given stream into a TdfNode.
+        /// </summary>
+        /// <param name="s">The stream to read from.</param>
+        /// <returns>A TdfNode containing the read data.</returns>
+        public static TdfNode LoadTdf(Stream s)
+        {
+            return LoadTdf(new StreamReader(s));
+        }
+
+        /// <summary>
+        /// Reads a TDF file from the given reader into a TdfNode.
+        /// </summary>
+        /// <param name="reader">The reader to read from.</param>
+        /// <returns>A TdfNode containing the read data.</returns>
+        public static TdfNode LoadTdf(TextReader reader)
+        {
+            var adapter = new TdfNodeAdapter();
+            var parser = new TdfParser(reader, adapter);
+            parser.Load();
+            return adapter.RootNode;
+        }
+
+        /// <summary>
+        /// Writes the contents of this TdfNode to the given stream
+        /// in TDF format.
+        /// </summary>
+        /// <param name="s">The stream to write to.</param>
+        public void WriteTdf(Stream s)
+        {
+            StreamWriter wr = new StreamWriter(s);
+            this.WriteTdf(wr, 0);
+            wr.Flush();
+        }
+        public bool ContentsEqual(TdfNode other)
+        {
+            if (!string.Equals(this.Name, other.Name, StringComparison.OrdinalIgnoreCase))
+            {
+                return false;
+            }
+            if (this.Keys.Count != other.Keys.Count)
+            {
+                return false;
+            }
+
+            if (this.Entries.Count != other.Entries.Count)
+            {
+                return false;
+            }
+
+            foreach (var entry in this.Entries)
+            {
+                if (!other.Entries.TryGetValue(entry.Key, out var value) || entry.Value != value)
+                {
+                    return false;
+                }
+            }
+
+            foreach (var entry in this.Keys)
+            {
+                if (!other.Keys.TryGetValue(entry.Key, out var value) || !entry.Value.ContentsEqual(value))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private void WriteTdf(StreamWriter writer, int depth)
+        {
+            string indent = new string(' ', depth * TdfNode.IndentationLevel);
+            string indent2 = new string(' ', (depth + 1) * TdfNode.IndentationLevel);
+
+            // write out the header
+            writer.Write(indent);
+            writer.WriteLine("[{0}]", this.Name);
+
+            // open the body
+            writer.Write(indent2);
+            writer.WriteLine("{");
+
+            // write the body
+            // first, variables and their values
+            foreach (var e in this.Entries)
+            {
+                writer.Write(indent2);
+                writer.WriteLine("{0}={1};", e.Key, e.Value);
+            }
+
+            // then subkeys
+            foreach (var e in this.Keys)
+            {
+                e.Value.WriteTdf(writer, depth + 1);
+            }
+
+            // close the body
+            writer.Write(indent2);
+            writer.WriteLine("}");
+        }
+    }
+}
---
+++ TA_Editor/Tdf/TdfParser.cs
@@ -0,0 +1,297 @@
+﻿namespace TAUtil.Tdf
+{
+    using System.Globalization;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+    /// <summary>
+    /// Class for parsing TDF format files.
+    /// </summary>
+    public class TdfParser
+    {
+        private const int EofSignal = -1;
+
+        private readonly TdfLexingReader reader;
+
+        private readonly ITdfNodeAdapter adapter;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TdfParser"/> class.
+        /// </summary>
+        /// <param name="s">The stream to read from.</param>
+        /// <param name="adapter">The object to pass parsed data to.</param>
+        public TdfParser(Stream s, ITdfNodeAdapter adapter)
+            : this(new StreamReader(s), adapter)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TdfParser"/> class.
+        /// </summary>
+        /// <param name="reader">The reader to read from.</param>
+        /// <param name="adapter">The object to pass parsed data to.</param>
+        public TdfParser(TextReader reader, ITdfNodeAdapter adapter)
+        {
+            this.reader = new TdfLexingReader(reader);
+            this.adapter = adapter;
+        }
+
+        /// <summary>
+        /// Gets the line number of the line that the parser is currently reading.
+        /// </summary>
+        public int CurrentLine
+        {
+            get
+            {
+                return this.reader.CurrentLine;
+            }
+        }
+
+        /// <summary>
+        /// Gets the column number of the character that the parser is currently reading.
+        /// </summary>
+        public int CurrentColumn
+        {
+            get
+            {
+                return this.reader.CurrentColumn;
+            }
+        }
+
+        /// <summary>
+        /// Reads and parses the input.
+        /// </summary>
+        public void Load()
+        {
+            this.adapter.Initialize(this);
+
+            this.OptionalWhitespace();
+
+            while (!this.Accept(EofSignal))
+            {
+                this.Block();
+                this.OptionalWhitespace();
+            }
+        }
+
+        private void Block()
+        {
+            var title = this.BlockTitle();
+
+            this.adapter.BeginBlock(title);
+
+            this.OptionalWhitespace();
+            this.BlockBody();
+
+            this.adapter.EndBlock();
+        }
+
+        private string BlockTitle()
+        {
+            this.Expect('[');
+            var s = this.BlockName();
+            this.Expect(']');
+
+            return s;
+        }
+
+        private string BlockName()
+        {
+            StringBuilder s = new StringBuilder();
+
+            char next = (char)this.Next();
+            while (this.AcceptNotAny(']', EofSignal))
+            {
+                s.Append(next);
+                next = (char)this.Next();
+            }
+
+            return s.ToString();
+        }
+
+        private void BlockBody()
+        {
+            this.Expect('{');
+            this.OptionalWhitespace();
+
+            while (!this.Accept('}'))
+            {
+                this.BlockBodyEntry();
+                this.OptionalWhitespace();
+            }
+        }
+
+        private void BlockBodyEntry()
+        {
+            if (this.Next() == '[')
+            {
+                this.Block();
+            }
+            else if (this.Accept(';'))
+            {
+                // Empty statement (i.e. terminator that terminates nothing).
+                // Strictly this isn't really valid,
+                // but some files in the wild do have this
+                // and we need to cope with them.
+            }
+            else
+            {
+                this.Property();
+            }
+        }
+
+        private void Property()
+        {
+            var name = this.PropertyName();
+            this.Expect('=');
+            var value = this.PropertyValue();
+            this.Terminator();
+
+            this.adapter.AddProperty(name, value);
+        }
+
+        private void Terminator()
+        {
+            if (!this.Accept(';'))
+            {
+                this.Error();
+            }
+        }
+
+        private string PropertyName()
+        {
+            var sb = new StringBuilder();
+            char next = (char)this.Next();
+
+            if (!this.AcceptNotAny('=', '\n', ';', EofSignal))
+            {
+                this.Error();
+            }
+
+            sb.Append(next);
+            next = (char)this.Next();
+
+            while (this.AcceptNotAny('=', '\n', ';', EofSignal))
+            {
+                sb.Append(next);
+                next = (char)this.Next();
+            }
+
+            return sb.ToString();
+        }
+
+        private string PropertyValue()
+        {
+            var sb = new StringBuilder();
+
+            char next = (char)this.Next();
+
+            while (this.AcceptNotAny(';', EofSignal))
+            {
+                sb.Append(next);
+                next = (char)this.Next();
+            }
+
+            return sb.ToString();
+        }
+
+        private void OptionalWhitespace()
+        {
+            while (this.AcceptWhitespace())
+            {
+                // keep accepting
+            }
+        }
+
+        private bool AcceptWhitespace()
+        {
+            return this.AcceptAny(' ', '\n', '\t');
+        }
+
+        private int Next()
+        {
+            return this.reader.Peek();
+        }
+
+        private void Consume()
+        {
+            this.reader.Read();
+        }
+
+        private void Error()
+        {
+            var msg = string.Format(
+                "Unexpected character '{0}'",
+                this.PrintableNextChar());
+            throw new TdfParseException(
+                msg,
+                this.CurrentLine,
+                this.CurrentColumn);
+        }
+
+        private string PrintableNextChar()
+        {
+            var next = this.Next();
+            if (next == EofSignal)
+            {
+                return "EOF";
+            }
+
+            return Regex.Escape(((char)next).ToString(CultureInfo.InvariantCulture));
+        }
+
+        private void Expect(int token)
+        {
+            if (!this.Accept(token))
+            {
+                this.Error();
+            }
+        }
+
+        private bool Accept(int token)
+        {
+            if (this.Next() != token)
+            {
+                return false;
+            }
+
+            this.Consume();
+            return true;
+        }
+
+        private bool AcceptNot(int token)
+        {
+            if (this.Next() == token)
+            {
+                return false;
+            }
+
+            this.Consume();
+            return true;
+        }
+
+        private bool AcceptAny(params int[] tokens)
+        {
+            if (tokens.Any(x => x == this.Next()))
+            {
+                this.Consume();
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool AcceptNotAny(params int[] tokens)
+        {
+            if (tokens.Any(x => x == this.Next()))
+            {
+                return false;
+            }
+
+            this.Consume();
+            return true;
+        }
+    }
+}
