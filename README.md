fd40c68de42dfd6d2e023ca5fccc33287df2fee9
Michael Heasell <michael.heasell@gmail.com>
2019-06-28 22:56:09 +0100

    Extract about box text to readme

--- README.md
+++ README.md
@@ -7,6 +7,38 @@ The original version can be found at the following location:
 
 https://www.tauniverse.com/forum/showthread.php?t=45855
 
+## Info
+
+This tool is designed to manipulate Total Annihilation data files.
+Before you can read any data you need to extract the .ufo, .hpi or .gp3
+files you want to manipulate. Use a HPI extracting tool, e.g. HPIView,
+to do this. When you have extracted all the files, you need to specify
+the folder path where you have extracted the files. This folder must
+contain a 'units' folder that contains .fbi files. If you want to edit
+weapon files, the folder must also contain a 'weapons' folder containing
+.tdf files.
+
+Some mods use custom weapon and unit folder names
+of the format 'unitsX' or 'weaponX', where X is any letter.
+These are also recognised by the tool.
+
+**Note:** It is recommended to make a backup of all unit and weapon
+files before editing.
+
+Click on the specific button to read unit or weapon files. Weapons are
+always corresponding to the shown units, so be sure to read some units
+first.
+
+Edit the values in the specific cells manually or use the calculation
+functions on the selected cells. After you are finished you need to save
+the edited data. This will update the unit and weapon files on disk to
+reflect the changes made in the table. The last step to do is to pack
+the folder back into a HPI archive and place it into your TA folder. Use
+a HPI packing tool, e.g. HPIPack, to do this.
+
+The original author of this program is Pascal Wauer. This fork contains
+modifications by Michael Heasell. Use this program at your own risk.
+
 ## Why a Fork?
 
 The original project has numerous bugs, particularly
--- TA_Editor/MainWindow.xaml.cs
+++ TA_Editor/MainWindow.xaml.cs
@@ -28,26 +28,13 @@ namespace TA_Editor
 
         private void OnInformationClick(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show(
-                "Info\r" +
-                "\r" +
-                "This tool is designed to manipulate Total Annihilation data files.\r" +
-                "Before you can read any data you need to extract the .ufo, .hpi or .gp3 files you want to manipulate (HPIviewer can help here). \r" +
-                "\r" +
-                "When you have extracted all the files, you need to specify the folder path where you have extracted the files. \r" +
-                "This folder must have a folder containing the word 'unit' including .fbi files. It must have a folder containg the word 'weapon' including .tdf files if you want to edit weapon files. \r"+
-                "It is suggested to make a backup of all WEAPONS and UNITS folders before starting to edit the files.\r" +
-                "\r" +
-                "Click on the specific button to read unit or weapon files. Weapons are always corresponding to the shown units. So be sure to read some units first. \r" +
-                "\r" +
-                "Edit the values in the specific cells manually or use the calculation functions on the selected cells.\r" +
-                "After you are finished you need to SAVE the edited data. This will replace all changed files in the original folder with the values in the table. \r" +
-                "The last step to do is to pack the folder back to its original file (HPIpacker can help here) and place it into your TA folder. \r" +
-                "\r" +
-                "The original author of this program is Pascal Wauer. This fork contains modifications by Michael Heasell. Using this program is under your own responsibility." +
-                "\r"
-                , "Total Annihilation Units and Weapons Editor " + Assembly.GetExecutingAssembly().GetName().Version
-                );
+            var version = Assembly.GetExecutingAssembly().GetName().Version;
+            var title = "About Total Annihilation Units and Weapons Editor";
+            var message =
+                $"Total Annihilation Units and Weapons Editor {version}"
+                + "\r\n"
+                + "\r\nThe original author of this program is Pascal Wauer. This fork contains modifications by Michael Heasell.";
+            MessageBox.Show(message, title);
         }
 
         private void OnSelectionChangedFBI(object sender, SelectedCellsChangedEventArgs e)
